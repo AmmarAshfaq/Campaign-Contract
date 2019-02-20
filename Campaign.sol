7a3e844d50c9737da9167478af948766e4746fd5
AmmarAshfaq <ammar_ashfaq@yahoo.com>
2019-02-20 14:50:35 -0800

    Campaign Contract

--- Campaign.sol
+++ Campaign.sol
@@ -6,11 +6,14 @@ contract Campaign {
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
         
     }
     address public manager;
     uint public minimumContribution;
-    address [] public approvers;
+    // address [] public approvers; //replace with mapping because mapping take constant times
+    mapping(address => bool) public approvers;
     Request [] public requests;
      modifier restricted(){
         require(msg.sender == manager);
@@ -28,17 +31,35 @@ contract Campaign {
     // every contract variable store in the storage area,argument variable are like a memory
     function contribute() public payable{
         require(msg.value >minimumContribution);
-        approvers.push(msg.sender);
+        // approvers.push(msg.sender);
+        approvers[msg.sender] = true;
     }
-    
+    // array is linear time search
+    // mapping is constant time search
+    // In mapping key are not stored.we are working on hashing function.In hash table we are having a loopup process.
+    //In mapping we create hashing through lookup process.where we assign key in hashing function and its return index of the value.
+    // In mapping values are not iterable means we cannot fetch all values
+    //In mapping all values exist which means if we assign a key in hash function it returns value instead of undefined.
     function createRequest(string description,uint value,address recipient) public restricted{
+        // we have only set the value here which is value type not refernce type
         Request memory newRequest = Request({
             description:description,
             value:value,
             recipient:recipient,
-            complete:false
+            complete:false,
+            approvalCount:0
             
         });
         requests.push(newRequest);
+    }
+    
+    
+    function approverRequest(uint index) public{
+        Request storage request = requests[index];
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+        
+        request.approvals[msg.sender] =true;
+        request.approvalCount++;
     }
 }
